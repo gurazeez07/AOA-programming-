1ae77edb2ad7f9aa888f8e0ea55fa755c3d0c754
JustinStri <jstrikowski@ufl.edu>
2023-02-26 20:03:14 -0500

    works, but runs in infinite loop, exceeds bounds

--- Strat2.py
+++ Strat2.py
@@ -9,22 +9,32 @@ for i in range(m):
     start, end = map(int, input().split())
     houses.append((start, end, i+1))
 
-houses.sort()
+houses.sort(key=lambda x: x[0], reverse=True)
 
 painted = 0
 currentDay = 1
-pq = PriorityQueue()
+index = 0
+addedOne = False
 
-for house in houses:
-    start, end, index = house
+while currentDay < n:
+    if index >= len(houses):
+        index = 0
+    house = houses[index]
+    start, end, i = house
     if start > currentDay:
-        currentDay = start
-        while not pq.empty() and pq.queue[0][0] < currentDay:
-            pq.get()
+        index += 1
+        continue
+        # currentDay = start
     if currentDay <= end and currentDay >= start:
-        pq.put((-end, index))
-    if not pq.empty():
-        _, index = pq.get()
-        painted += 1
+        print(house[2])
+        houses.remove(house)
+        index = 0
         currentDay += 1
-        print(index)
+        addedOne = True
+        continue
+    else:
+        index += 1
+        currentDay += 1
+    # if index == len(houses):
+    #     break
+    
