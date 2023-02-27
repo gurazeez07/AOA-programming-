c49e20af64e1ac4b105aba0fba3550d50204b5af
JustinStri <jstrikowski@ufl.edu>
2023-02-27 12:32:03 -0500

    no longer uses unavailable houses

--- Strat3.py
+++ Strat3.py
@@ -1,5 +1,6 @@
 import heapq
 
+# Paint the shortest duration house first
 n, m = map(int, input().split())
 houses = []
 for i in range(m):
@@ -13,12 +14,15 @@ available = []  # heap of available unpainted houses sorted by end day
 
 for day in range(1, n+1):
     while houses and houses[0][0] == day:
-        _, end, index = houses.pop(0)
-        heapq.heappush(available, (end, index))
+        start, end, index = houses.pop(0)
+        heapq.heappush(available, (end - start, end, index))
 
-    if available:
-        end, index = heapq.heappop(available)
+    while len(available) > 0:
+        duration, end, index = heapq.heappop(available)
+        if end < day:
+            continue
         painted.append(index)
+        break
 
 print('\n'.join(map(str, painted)))
 
