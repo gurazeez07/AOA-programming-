c92c7b131fda011836f701964929193a646eb386
JustinStri <jstrikowski@ufl.edu>
2023-02-27 01:12:11 -0500

    changes made to make algorithm simpler

--- Strat2.py
+++ Strat2.py
@@ -14,23 +14,31 @@ houses.sort(key=lambda x: x[0], reverse=True)
 painted = 0
 currentDay = 1
 index = 0
-addedOne = False
+# addedOne = False
 
 while (currentDay < n) & (len(houses) > 0):
     if index >= len(houses):
+        currentDay += 1
         index = 0
+
     house = houses[index]
     start, end, i = house
+
+    # increment if not at right start yet
     if start > currentDay:
         index += 1
         continue
+    if end < currentDay:
+        houses.remove(house)
+        continue
         # currentDay = start
+
+    # if can paint the house today
     if currentDay <= end and currentDay >= start:
         print(house[2])
         houses.remove(house)
         index = 0
         currentDay += 1
-        addedOne = True
         continue
     else:
         index += 1
