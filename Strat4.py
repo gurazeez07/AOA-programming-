565d4a151a16caba74da62b55f9538deab0525ac
Gurazeez Singh Sachdeva <66404378+gurazeez07@users.noreply.github.com>
2023-02-26 19:04:20 -0500

    Update Strat4.py

--- Strat4.py
+++ Strat4.py
@@ -1,28 +1,31 @@
-
 import heapq
 
-n, m = map(int, input().split())
-houses = []
-for i in range(m):
-    start_day, end_day = map(int, input().split())
-    houses.append((start_day, end_day, i+1))
-
-houses.sort()
-
-painted = 0
-current_day = 1
-available_houses = []
-for i in range(m):
-    start_day, end_day, house_idx = houses[i]
-    if start_day > current_day:
-        current_day = start_day
-        while available_houses and available_houses[0][0] < current_day:
-            heapq.heappop(available_houses)
-    if current_day <= end_day:
-        heapq.heappush(available_houses, (end_day, house_idx))
-    if available_houses:
-        _, painted_house_idx = heapq.heappop(available_houses)
-        painted += 1
-        print(painted_house_idx)
-
-print(painted)
+def strat(n, m, houses):
+    pq = []  # priority queue to store unpainted houses available on a day
+    painted = [0] * m  # array to store painted houses
+    output = []  # list of house indices to paint
+    
+    for i in range(1, n+1):
+        # Add all the unpainted houses that are available on the current day to the priority queue.
+        while houses and houses[0][0] <= i:
+            start, end, index = houses.pop(0)
+            if not painted[index]:
+                heapq.heappush(pq, (end, index))  # push the unpainted house into the priority queue
+        
+        # Paint the house that will stop being available the earliest among the unpainted houses available on the current day.
+        if pq:
+            end, index = heapq.heappop(pq)
+            painted[index] = 1
+            output.append(index)
+    
+    # Print the output list of house indices.
+    for index in output:
+        print(index+1, end=' ')  # house indices are 1-based
+    
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    houses = []
+    for i in range(m):
+        start, end = map(int, input().split())
+        houses.append((start, end, i))
+    strat(n, m, houses)
