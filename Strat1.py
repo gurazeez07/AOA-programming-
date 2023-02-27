5f0a992e1316eb5cba8d6cd13fb6ae9465b82791
JustinStri <jstrikowski@ufl.edu>
2023-02-27 00:17:58 -0500

    notes and code changes

--- Strat1.py
+++ Strat1.py
@@ -1,31 +1,30 @@
 import sys
 
-# Read input from standard input
+# Get input from file // command line??
 n, m = map(int, input().split())
 houses = []
 for i in range(m):
     start, end = map(int, input().split())
     houses.append((start, end, i+1))
 
-# Sort houses by start day and end day
+# Sort houses by earliest start date
 houses.sort()
 
-# Initialize variables
 painted = []
 currentDay = 1
 
 # Iterate over houses
 for house in houses:
     start, end, i = house
-    # If we need to wait for the next available day to paint the house
+    # If we need to wait for the next free day to paint
     if start > currentDay:
         currentDay = start
-    # If we can paint the house on the current day
+    # If can paint the house today
     if currentDay <= end:
         painted.append(i)
         currentDay += 1
 
 # Output the indices of painted houses
 for i in painted:
-    print(i, end=' ')
+    print(i + ' ')
 print()
