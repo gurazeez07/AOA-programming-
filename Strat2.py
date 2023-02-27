88f3656263c92154ce770f2f028477b2af3c8742
JustinStri <jstrikowski@ufl.edu>
2023-02-27 13:20:15 -0500

    -end not needed

--- Strat2.py
+++ Strat2.py
@@ -18,12 +18,12 @@ for day in range(1, n+1):
     # Add all the unpainted houses that are available on the current day to the priority queue.
     while houses and houses[0][0] == day:
         start, end, index = houses.pop(0)
-        stack.append((-start, -end, index))
+        stack.append((-start, end, index))
 
     # Paint the house that became available the latest among the unpainted houses available on the current day.
     while not len(stack) == 0:
         start, end, index = stack.pop()
-        if -end < day:
+        if end < day:
             continue
         print(index)
         break
